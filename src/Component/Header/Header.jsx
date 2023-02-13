1c4e4f02510f09085fcc25003ef1d4df471200a0
Raza <71090111+Raza2468@users.noreply.github.com>
2023-02-14 03:55:58 +0500

    MGALink

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -23,8 +23,9 @@ import FloodHeader from '../Pages/Floot/FloodHeader';
 
 
 function Header(props) {
-    const [resultsPerPage, setResultsPerPage] = useState("/");
-    console.log(resultsPerPage);
+    const [resultsPerPage, setResultsPerPage] = useState("home");
+    const [MGAOptions, setMGAOptions] = useState("");
+    // console.log(resultsPerPage);
     const [openMenu, setOpenMenu] = useState("large")
     return (
         <div>
@@ -53,6 +54,7 @@ function Header(props) {
                         justifyContent="flex-start"
                         onClick={() => setResultsPerPage('home')}> Home</Button>
                     {/* </Link> */}
+
                     <Select placeholder='New Quoto' color='white'
                         value={resultsPerPage} onChange={(event) => setResultsPerPage(event.target.value)}
                     >
@@ -60,7 +62,7 @@ function Header(props) {
                         <option value='/BuilderRisk' style={{ color: "#2347F0", fontWeight: "bold" }}>Builders Risk</option>
                         <option value='/CanineLabHeader' style={{ color: "#2347F0", fontWeight: "bold" }}>Canine Laibility</option>
                         <option value='option4' style={{ color: "#2347F0", fontWeight: "bold" }}>Old MErc GL</option>
-                        <option value='option5' style={{ color: "#2347F0", fontWeight: "bold" }}>Commerical GL</option>
+                        <option value='' style={{ color: "#2347F0", fontWeight: "bold" }}>Commerical GL</option>
                         <option value='option6' style={{ color: "#2347F0", fontWeight: "bold" }}>Commerical Property</option>
                         <option value='option7' style={{ color: "#2347F0", fontWeight: "bold" }}>Commerical Package</option>
                         <option value='option8' style={{ color: "#2347F0", fontWeight: "bold" }}>Home Owners</option>
@@ -78,7 +80,40 @@ function Header(props) {
                         <option value='option19' style={{ color: "#2347F0", fontWeight: "bold" }}>Professional Liapility</option>
                     </Select>
                     <Button w="100%" justifyContent="flex-start" bg="#2347F0" color="white" _hover={{ bg: "#0B67FB" }} _focus={{ boxShadow: "outline", color: "white" }}><Link href='http://clickfinancing.net/' isExternal >Click Financing</Link></Button>
-                    <Button w="100%" justifyContent="flex-start" bg="#2347F0" color="white" _hover={{ bg: "#0B67FB" }} _focus={{ boxShadow: "outline", color: "white" }}>MGA Links</Button>
+                    {/* <Button w="100%" justifyContent="flex-start" bg="#2347F0" color="white" _hover={{ bg: "#0B67FB" }} _focus={{ boxShadow: "outline", color: "white" }}>MGA Links</Button> */}
+                    <Select placeholder='MGA Links' color='white'
+                        value={MGAOptions} onChange={(event) => window.open(event.target.value)}
+                    >
+
+
+                        <option value='option1' style={{ color: "#2347F0", fontWeight: "bold" }}>Administer</option>
+                        <option value='https://cloud.docstar.com/' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='https://cloud.docstar.com/' isExternal >Docstar Eclipse</Link></option>
+                        <option value='https://login.siteinspections.com/login.aspx?ReturnUrl=%2f' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Site Inspections</Link></option>
+                        <option value='https://www.castledataservices.com/CastleClient/login.aspx' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Castle Inspections</Link></option>
+                        <option value='http://aces.atlanticcasualty.net/aci_web/login.aspx' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Atlantic Casualty</Link></option>
+                        <option value='https://www.argolimited.com/pages/argo-group-home/our-brands/colony-specialty' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Colony</Link></option>
+                        <option value='http://npic.com/agents.html' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >North Pointe</Link></option>
+                        <option value='https://escape.torusinsurance.com/excess/default.aspx' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Torus</Link></option>
+                        <option value='https://apps8.fldfs.com/proofofcoverage/Search.aspx' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >FL Work Comp lookup</Link></option>
+                        <option value='https://www.myfloridalicense.com/wl11.asp?mode=0&amp;SID=' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >FL contractor license search</Link></option>
+                        <option value='http://www.portal.state.pa.us/portal/server.pt?open=514&amp;objID=552709&amp;mode=2' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >PA Wok Comp lookup</Link></option>
+                        <option value='https://www.ewccv.com/cvs/?ref=https%3A%2F%2Fsbwc.georgia.gov%2Fonline-employers-workers-compensation-coverage-verification' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Georgia Work Comp lookup</Link></option>
+                        <option value='https://ecorp.sos.ga.gov/BusinessSearch' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Georgia Div of Corporations</Link></option>
+                        <option value='http://verify.sos.ga.gov/verification/Search.aspx' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Georgia contractor license search</Link></option>
+                        <option value='' style={{ color: "#2347F0", fontWeight: "bold" }}>Evolve Links</option>
+                        <option value='http://www.lloyds.com/the-market/tools-and-resources/tools-e-services/binding-authority-registration-bar' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >BAR</Link></option>
+                        <option value='http://www.lloyds.com/The-Market/Tools-and-Resources/Tools-E-Services/Atlas' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >ATLAS</Link></option>
+                        <option value='https://lloydscanada.com/' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Lineage </Link></option>
+                        <option value='http://www.lloyds.com/The-Market/Tools-and-Resources/Tools-E-Services/QA-Tool/Binding-Authority-QA-Tool' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >BA Quality Assurance</Link></option>
+                        <option value='http://www.lloyds.com/The-Market/Tools-and-Resources/Tools-E-Services/Crystal' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Crystal</Link></option>
+                        <option value='http://www.lloyds.com/the-market/tools-and-resources/tools-e-services/qa-tool/open-market-qa-tool' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >OM Quality Assurance</Link></option>
+                        <option value='https://www.lloydswordings.com/lma/auth/login.do?req_url=https://www.lloydswordings.com/lma/app/start.do' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Wordings Reposity</Link></option>
+                        <option value='https://telussecurecontracts.com/flex/login.jsp' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='https://telussecurecontracts.com/flex/login.jsp' isExternal >Attorney In Fact</Link></option>
+                        <option value='https://www.liiba.co.uk/liiba/default.aspx' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >LIIBA</Link></option>
+                        <option value='http://www.lloyds.com/' style={{ color: "#2347F0", fontWeight: "bold" }}><Link href='' isExternal >Lloyd’s</Link></option>
+                    </Select>
+
+
                     <Button w="100%" justifyContent="flex-start" bg="#2347F0" color="white" _hover={{ bg: "#0B67FB" }} _focus={{ boxShadow: "outline", color: "white" }} onClick={() => setResultsPerPage('ClientSearch')}>Client Search</Button>
                     <Button w="100%" justifyContent="flex-start" bg="#2347F0" color="white" _hover={{ bg: "#0B67FB" }} _focus={{ boxShadow: "outline", color: "white" }} onClick={() => setResultsPerPage('FileAclaim')}>File a Claim</Button>
                     {/* <Button w="100%" justifyContent="flex-start" bg="#2347F0" color="white" _hover={{ bg: "#0B67FB" }} _focus={{ boxShadow: "outline", color: "white" }}>Click Financing</Button> */}
@@ -141,12 +176,13 @@ function Header(props) {
                 resultsPerPage === '/GlArtisan' ? (<><GlArtisan /></>) :
                     resultsPerPage === '/BuilderRisk' ? (<><BuilderRiskHeader /></>) :
                         resultsPerPage === '/APDheader' ? (<><APDheader /></>) :
-                        resultsPerPage === '/MTCheader' ? (<><MTCheader /></>) :
-                        resultsPerPage === '/FloodHeader' ? (<><FloodHeader /></>) :
-                            resultsPerPage === '/CanineLabHeader' ? (<><CanineLabHeader /></>) :
-                                (resultsPerPage === 'ClientSearch') ? (<><ClientSearch /></>) :
-                                    (resultsPerPage === 'FileAclaim') ? (<><FileAclaim /></>)
-                                        : <></>
+                            resultsPerPage === '/MTCheader' ? (<><MTCheader /></>) :
+                                resultsPerPage === '/FloodHeader' ? (<><FloodHeader /></>) :
+                                    resultsPerPage === '/CanineLabHeader' ? (<><CanineLabHeader /></>) :
+                                        (resultsPerPage === 'ClientSearch') ? (<><ClientSearch /></>) :
+                                            (resultsPerPage === 'FileAclaim') ? (<><FileAclaim /></>) :
+                                                (MGAOptions === 'option3') ? (<>{console.log(MGAOptions, "raza")}</>)
+                                                    : <></>
             }
         </div>
     );
